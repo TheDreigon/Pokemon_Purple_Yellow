088f7524d33ae4da0220ac862379632481d21573
TheDreigon <33382293+TheDreigon@users.noreply.github.com>
2026-04-08 02:53:32 +0100

    [world design] Third round of Route and City map polishes and creative liberties. Increase Pallet Town 1 block down; Route 10, Lavender Town 1 block right.

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -16,11 +16,11 @@ ENDM
 ; Each map also has associated data in maps.asm.
 ; Order: towns/cities, then routes, then indoor/dungeon maps
 	const_def
-	map_const PALLET_TOWN,                   10,  9 ; $00
+	map_const PALLET_TOWN,                   10, 10 ; $00
 	map_const VIRIDIAN_CITY,                 20, 18 ; $01
 	map_const PEWTER_CITY,                   20, 18 ; $02
 	map_const CERULEAN_CITY,                 20, 18 ; $03
-	map_const LAVENDER_TOWN,                 10,  9 ; $04
+	map_const LAVENDER_TOWN,                 11,  9 ; $04
 	map_const VERMILION_CITY,                20, 18 ; $05
 	map_const CELADON_CITY,                  25, 18 ; $06
 	map_const FUCHSIA_CITY,                  20, 18 ; $07
@@ -39,7 +39,7 @@ DEF FIRST_ROUTE_MAP EQU const_value
 	map_const ROUTE_7,                       10,  9 ; $12
 	map_const ROUTE_8,                       30,  9 ; $13
 	map_const ROUTE_9,                       30,  9 ; $14
-	map_const ROUTE_10,                      10, 36 ; $15
+	map_const ROUTE_10,                      11, 36 ; $15
 	map_const ROUTE_11,                      30,  9 ; $16
 	map_const ROUTE_12,                      10, 54 ; $17
 	map_const ROUTE_13,                      30,  9 ; $18
