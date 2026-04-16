e7b5b6a879b466e88f7c5d42f30e98a0ac26fed0
TheDreigon <33382293+TheDreigon@users.noreply.github.com>
2026-04-17 00:30:20 +0100

    Test drive - revert to normal.

--- data/pokemon/base_stats/eevee.asm
+++ data/pokemon/base_stats/eevee.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/eevee.pic", 0, 1 ; sprite dimensions
 	dw EeveePicFront, EeveePicBack
 
-	db WILL_O_WISP, BULK_UP, FLASH, ICE_BEAM ; level 1 learnset
+	db TACKLE, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
