1fbc73f0292134c08cc86f371e0969e5af767a97
TheDreigon <33382293+TheDreigon@users.noreply.github.com>
2026-08-06 07:29:39 +0100

    Polish Celadon city and Celadon hotel.

--- data/maps/objects/CeladonHotel.asm
+++ data/maps/objects/CeladonHotel.asm
@@ -7,14 +7,14 @@ CeladonHotel_Object:
 	db $0 ; border block
 
 	def_warp_events
-	warp_event  3,  7, LAST_MAP, 13
-	warp_event  4,  7, LAST_MAP, 13
+	warp_event  5,  7, LAST_MAP, 13
+	warp_event  6,  7, LAST_MAP, 13
 
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_GRANNY, STAY, DOWN, TEXT_CELADONHOTEL_GRANNY
-	object_event  2,  4, SPRITE_BEAUTY, STAY, NONE, TEXT_CELADONHOTEL_BEAUTY
-	object_event  8,  4, SPRITE_SUPER_NERD, WALK, LEFT_RIGHT, TEXT_CELADONHOTEL_SUPER_NERD
+	object_event  5,  1, SPRITE_GRANNY, STAY, DOWN, TEXT_CELADONHOTEL_GRANNY
+	object_event  2,  3, SPRITE_BEAUTY, STAY, NONE, TEXT_CELADONHOTEL_BEAUTY
+	object_event  3,  5, SPRITE_SUPER_NERD, WALK, LEFT_RIGHT, TEXT_CELADONHOTEL_SUPER_NERD
 
 	def_warps_to CELADON_HOTEL
