0b1a182ac8a8866ea29cf786ed32b364e54f26d7
TheDreigon <33382293+TheDreigon@users.noreply.github.com>
2026-08-10 19:55:18 +0100

    Quick touch on grimer and koffing stats.

--- data/pokemon/base_stats/grimer.asm
+++ data/pokemon/base_stats/grimer.asm
@@ -1,7 +1,7 @@
 	db DEX_GRIMER ; pokedex id
 	base_stat_row 80, 60, 50, 65, 30 ; hp atk def spc spd
 	db POISON, WATER ; type
-	db 190 ; catch rate  ; set by hand: sludge: less to hold than it looks
+	db 180 ; catch rate  ; set by hand: sludge: less to hold than it looks
 	db 90 ; base exp
 
 	INCBIN "gfx/pokemon/front/grimer.pic", 0, 1 ; sprite dimensions
