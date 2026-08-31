79a961ec81a571f79722d7ed8e478f3d95a750a4
TheDreigon <33382293+TheDreigon@users.noreply.github.com>
2026-08-31 23:00:24 +0100

    Polish Rhydon-line moveset.

--- data/pokemon/base_stats/rhydon.asm
+++ data/pokemon/base_stats/rhydon.asm
@@ -8,7 +8,7 @@
 	INCBIN "gfx/pokemon/front/rhydon.pic", 0, 1 ; sprite dimensions
 	dw RhydonPicFront, RhydonPicBack
 
-	db HORN_ATTACK, GROUND_STOMP, NO_MOVE, NO_MOVE ; level 1 learnset
+	db LEER, TACKLE, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
