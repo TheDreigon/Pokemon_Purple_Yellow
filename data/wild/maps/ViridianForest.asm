66ac203cc26450210b56b0f1cf53ef97bb1fcf8a
TheDreigon <33382293+TheDreigon@users.noreply.github.com>
2026-09-02 02:41:22 +0100

    Viridian Forest encounters.

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -2,14 +2,14 @@ ViridianForestWildMons:
 	def_grass_wildmons 30 ; encounter rate
 	db  4, CATERPIE
 	db  4, WEEDLE
-	db  6, ODDISH
+	db  5, VENONAT
 	db  6, BELLSPROUT
-	db  8, METAPOD
-	db  8, KAKUNA
-	db 11, BUTTERFREE
-	db 11, BEEDRILL
-	db  6, VENONAT
-	db  8, VENONAT
+	db  6, ODDISH
+	db  5, PARAS
+	db  8, PINSIR
+	db  5, VENONAT ; HERACROSS
+	db  8, SCYTHER
+	db  8, SCYTHER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
